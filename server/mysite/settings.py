1f5612a710211ec5f325c3eca161a113d66dd4ab
Ishank307 <ishankkumar307@gmail.com>
2025-12-12 23:54:10 +0530

    all changes till now

--- server/mysite/settings.py
+++ server/mysite/settings.py
@@ -48,10 +48,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'bookings',
     'rest_framework',
-
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -149,3 +150,10 @@ RAZORPAY_WEBHOOK_SECRET = os.environ.get('RAZORPAY_WEBHOOK_SECRET')
 
 # Partial Payment Configuration
 PARTIAL_PAYMENT_PERCENTAGE = 30 # Percentage of total amount for partial payment
+
+# CORS Settings
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+CORS_ALLOW_CREDENTIALS = True
