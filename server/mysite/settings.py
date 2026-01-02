48b53afdd469ce570d3018485242074ea0e5df78
Sheikh Umaid <sheikhumaid@pm.me>
2026-01-02 12:24:14 +0530

    cors

--- server/mysite/settings.py
+++ server/mysite/settings.py
@@ -185,14 +185,6 @@ RAZORPAY_WEBHOOK_SECRET = os.environ.get('RAZORPAY_WEBHOOK_SECRET')
 # Partial Payment Configuration
 PARTIAL_PAYMENT_PERCENTAGE = 25 # Percentage of total amount for partial payment
 
-# CORS Settings
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-]
-CORS_ALLOW_CREDENTIALS = True
-
-
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
